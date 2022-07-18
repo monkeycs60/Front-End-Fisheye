977ba117de397f2dc2f949681813188fc7f7c2bd
monkeycs60 <clement.serizay@gmail.com>
2022-07-18 22:41:45 +0200

    test fetch javascript

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -2,6 +2,22 @@ function photographerFactory(data) {
     const { name, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
+    //fetch datas from photographer.json and log them in console
+    fetch(`/Front-End-Fisheye/data/photographers.json`)
+    .then((response) => response.json())
+    .then((data) => console.log(data.photographers[0].name))
+    //create a new object with the datas of the photographer
+    //log the name of each photographer
+    .then((data) => console.log(data.photographers));
+    
+
+    //     .then(data => console.log(data))
+    //    //log the name of all photographers in the console
+       
+    //     .then(data => console.log(data.body))
+
+  
+
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
