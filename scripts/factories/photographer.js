88433d47f8f1f0973590a7e709ed350cb02e4202
monkeycs60 <clement.serizay@gmail.com>
2022-07-20 15:51:33 +0200

    css éléments fetch

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -36,6 +36,7 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        h2.classList.add("name");
         article.appendChild(img);
         article.appendChild(h2);
         return (article);
