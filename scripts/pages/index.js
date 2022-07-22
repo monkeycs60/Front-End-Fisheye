21fe3d6bee58c0f72aa636a35c2c7e8631e1588c
monkeycs60 <clement.serizay@gmail.com>
2022-07-22 15:40:28 +0200

    image radius + sémantique + access

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -48,5 +48,3 @@ async function init() {
 }
 
 init();
-
-
--- scripts/utils/API.js
+++
@@ -1,9 +0,0 @@
-fetch("/Front-End-Fisheye/data/photographers.json")
-.then((response) => response.json())
-.then((data) => {
-console.log(data.photographers);
-return data.photographers;
-
-}).catch((error) => {
-    console.log(error);
-});
