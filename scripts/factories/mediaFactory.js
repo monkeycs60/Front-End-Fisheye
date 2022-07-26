836c2cd052c00b7a20ad13053c80d341dc2b7ccd
monkeycs60 <clement.serizay@gmail.com>
2022-07-26 17:31:30 +0200

    fin de la dfactory : bug à corriger

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -18,11 +18,19 @@
     const photoSection = document.querySelector(".photographer_section");
    //CREATE N IMG ELEMENTS CORRESPOND TO DATA.LENGTH and fill each img element with the data.image
   for (let index = 0; index < data.length; index++) {
-   
-      const img = document.createElement("img");
+   //if data contains .mp4 in its title, create a video element
+    if (data[index].includes(".mp4")) {
+        const video = document.createElement("video");
+        video.setAttribute("src", `/Front-End-Fisheye/assets/SamplePhotos/${data[index]}`);
+        video.setAttribute("controls", "controls");
+        photoSection.appendChild(video);
+    }
+     else {
+         const img = document.createElement("img");
       img.setAttribute("src", `/Front-End-Fisheye/assets/SamplePhotos/${data[index]}`);
       img.setAttribute("alt", `cool`);
       photoSection.appendChild(img);
+     }
     
   }
         }
@@ -35,20 +43,7 @@
 
 }
 
-    //foreach entry of data, create a new img element
-    
-
-
-//    const dest = data.map((data) => {
-//         const img = document.createElement("img");
-//         img.setAttribute("src", `/Front-End-Fisheye/assets/SamplePhotos/${data}`);
-//         img.setAttribute("alt", `photo de Jean`);
-//         return img;
-        
-//     }
-   
-//     );
-   
+  
   
 
   
@@ -58,33 +53,3 @@
 
     
 
-
-//     function photographersGallery() {
-//         const img = document.createElement( 'img' );
-//         img.setAttribute("src", picture)
-//         img.setAttribute("alt", `photo ${image}`);
-//         //set width to 33%
-//         img.style.width = "33%";
-//         img.style.height = "33%";
-//         return (img);
-//     }
-
-//     function photographersMovie() {
-//         const video = document.createElement( 'video' );
-//         video.setAttribute("src", movie)
-//         video.setAttribute("alt", `video ${video}`);
-//         //set width 400px and height 300px
-//         video.setAttribute("width", "400");
-//         video.setAttribute("height", "300");
-//         return (video);
-//     }
-
-//     // if (image !== undefined) {
-//     //     return { photographersGallery }
-//     // }
-//     // if (video !== undefined) {
-//     //     return { photographersMovie }
-//     // }
-//     return { createImageFromArray}
-//     // return {   photographersGallery, photographersMovie }  
-// }
