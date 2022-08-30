3d77d4a0f29d274f53d9153b48d9940d0dc98a5a
monkeycs60 <clement.serizay@gmail.com>
2022-08-30 23:17:38 +0200

    mise à jour fetch dynamique url

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -7,7 +7,8 @@ async function getPhotographers() {
   let photographers = [];
   // await fetch("/Front-End-Fisheye/data/photographers.json")
   // await fetch("./../../data/photographers.json")
-  await fetch("../../data/photographers.json")
+  console.log(window.location.origin);
+  await fetch(`${window.location.origin}/data/photographers.json`)
     .then((response) => response.json())
     .then((data) => {
       photographers = data.photographers;
