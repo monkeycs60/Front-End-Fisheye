eeeceaaf6dfefb3b3bc19d8b1b14420f09470dea
monkeycs60 <clement.serizay@gmail.com>
2022-08-31 01:19:18 +0200

    media redirection gh pages

--- scripts/factories/3mediatypefactory.js
+++ scripts/factories/3mediatypefactory.js
@@ -10,15 +10,26 @@ export function mediaPhotographerFactory(data) {
       article.classList.add("grid-div");
       photoSection.appendChild(article);
 
+// create link for local and gh pages
+ let lienExterneMedia = `${window.location.origin}/assets/samplephotos/${data[index]}`;
+ if (!lienExterneMedia.includes("192.168")) {
+   lienExterneMedia = lienExterneMedia.replace(
+     "/assets",
+     "/Front-End-Fisheye/assets"
+   );
+ }
+
       // if data contains .mp4 in its title, create a video element
       if (data[index].includes(".mp4")) {
         const video = document.createElement("video");
-        video.setAttribute("src", `./../../assets/samplephotos/${data[index]}`);
+        // video.setAttribute("src", `./../../assets/samplephotos/${data[index]}`);  
+        video.setAttribute("src", lienExterneMedia);  
         article.appendChild(video);
         video.setAttribute("data-video", "true");
       } else {
         const img = document.createElement("img");
-        img.setAttribute("src", `./../../assets/samplephotos/${data[index]}`);
+        // img.setAttribute("src", `./../../assets/samplephotos/${data[index]}`);
+        img.setAttribute("src", lienExterneMedia);
         img.setAttribute("data-image", "true");
         article.appendChild(img);
       }
